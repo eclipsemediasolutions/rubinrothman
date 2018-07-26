b04274dd564044d49421b7547678e96e4dc17d1c
Kyle Roden <kyle@eclipsemediasolutions.com>
2018-07-26 11:57:46 -0700

    ie10 class

--- js/js-custom.js
+++ js/js-custom.js
@@ -135,8 +135,10 @@ $('#payment-online').waypoint({
 // 	},
 // 	offset: '85%'
 // });
-
-
+var ieVersion = /*@cc_on (function() {switch(@_jscript_version) {case 1.0: return 3; case 3.0: return 4; case 5.0: return 5; case 5.1: return 5; case 5.5: return 5.5; case 5.6: return 6; case 5.7: return 7; case 5.8: return 8; case 9: return 9; case 10: return 10;}})() || @*/ 0;
+if (ieVersion == 10) {
+  $('body').addClass('ie10');
+}
 /**
 ** ** ** **
 **
